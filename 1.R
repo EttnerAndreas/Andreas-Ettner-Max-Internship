d822a3d06ecac2ac11beb86d58e870b9fef744da
EttnerAndreas <andi.ettner@yahoo.de>
2020-09-16 13:23:10 +0200

    asd

---
+++ 1.R
@@ -0,0 +1,19 @@
+install.packages("git2r")
+
+library(git2r)
+
+2+2
+a <- c("1", "2", "3", "4")
+a
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git_congi
+git config --global user.name "andreasettner"
+git config --global user.email "andi.ettner@yahoo.de"
+
+
+?user.name
+
+x <- system("whoami", intern=TRUE)
+x
+email = andi.ettner@yahoo.de
