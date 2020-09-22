fa6d67299d6e5ba7b889a5d609835b5045c39746
EttnerAndreas <andi.ettner@yahoo.de>
2020-09-22 15:17:40 +0200

    Job-Function /NICHT

--- Loop_f_loop.R
+++ Loop_f_loop.R
@@ -1,12 +1,78 @@
 
+library(dplyr)
+library(ggplot2)
+library(Metrics)
+library(corpcor)
+library(nlme)
+library(MVN)
+library(DHARMa)
+library(useful)
+library(glmmTMB)
+library(usethis)
+library(devtools)
+library(knitr)
+library(coda)
+library(rjags)
+library(mvtnorm)
+library(rjags)
+library(R2jags)
+library(mvtnorm)
+
+
+side = 10
+global.mu = 0
+lambda = 0.2 
+
+
+dist.matrix <- function(side)
+{
+  row.coords <- rep(1:side, times=side)
+  col.coords <- rep(1:side, each=side)
+  row.col <- data.frame(row.coords, col.coords)
+  D <- dist(row.col, method="euclidean", diag=TRUE, upper=TRUE)
+  D <- as.matrix(D)
+  return(D)
+}
+
+dist.matrix <- function(side)
+{
+  row.coords <- rep(1:side, times=side)
+  col.coords <- rep(1:side, each=side)
+  row.col <- data.frame(row.coords, col.coords)
+  D <- dist(row.col, method="euclidean", diag=TRUE, upper=TRUE)
+  D <- as.matrix(D)
+  return(D)
+}
+
+cor.surface <- function(side, global.mu, lambda)
+{
+  D <- dist.matrix(side)
+  # scaling the distance matrix by the exponential decay
+  SIGMA <- exp(-lambda*D)
+  mu <- rep(global.mu, times=side*side)
+  # sampling from the multivariate normal distribution
+  M <- matrix(nrow=side, ncol=side)
+  M[] <- rmvnorm(1, mu, SIGMA)
+  return(M) # list(...)
+}
+
+side=10
+global.mu = 0
+lambda = 0.2 
+methods = c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN","Brent")
+M <- cor.surface(side = side, lambda = lambda, global.mu = global.mu)
+jag1 <- as.vector(as.matrix(M))
+my.data <- list(N = side * side, D = dist.matrix(side), y = jag1)
+
+
 counter=1
 
-for (g in 1:1 ){
+for (g in 1:5 ){
   time_repeat = 
     system.time({
       #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
       # Loop, wiederhole Experiment 5 mal für die Statistik
-      for (h in 1:2 ){
+      for (h in 1:10 ){
         time_glmmTMB = 
           system.time({
             lambda = 0 + (h*0.2)
@@ -15,7 +81,7 @@ for (g in 1:1 ){
             #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
             # Loop, ändere Lambda von lambda=0.2 in 0.05 Schritten bis Lambda=0.5
             
-            for (j in 1:2){
+            for (j in 1:10){
               time_side = 
                 system.time({
                   side = 2+ 2*j
@@ -38,25 +104,22 @@ for (g in 1:1 ){
                   new.data$group   <- as.factor(rep(1, new.data$N))
                   new.data$rows    <- new.data$row.col[,1]
                   new.data$cols    <- new.data$row.col[,2]
-                  
                   new.data1 = data.frame(resp = new.data$y)
                   new.data1$pos    <- numFactor(new.data$col, new.data$row.col)
                   new.data1$group  <- factor(rep(1, new.data$N))
                   new.data1$x      <- new.data$row.col
                   new.data1$y      <- new.data$col
                   #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-                  for(i in 1:1){
+                  for(i in 1:50){
                     data
                     time_glmmTMB = 
                       system.time({
                         fit.exp <- glmmTMB(resp ~ 1 + exp(pos + 0 | group), data = new.data1)
                       })
-
                     time_gls = 
                       system.time({
                         gls <- gls(y ~ 1, correlation=corExp (form =~ rows + cols), data = new.data)
                       })
-                    
                     time_optim = 
                       system.time({
                         ll = function(par) {
@@ -79,8 +142,8 @@ for (g in 1:1 ){
                   repeat.experiment[counter, 9] <- summary(gls)$coefficients
                   
                   repeat.experiment[counter, 10]  <- time_optim[3]
-                  repeat.experiment[counter, 11] <- result$value 
-                  repeat.experiment[counter, 12] <- result$par[1]
+                  repeat.experiment[counter, 11] <- result$par[1] 
+                  repeat.experiment[counter, 12] <- result$par[2]
                   
                   counter = counter + 1
                   
@@ -100,42 +163,21 @@ for (g in 1:1 ){
 
 repeat.experiment
 
-saveRDS(repeat.experiment, file = "loop2")
+saveRDS(repeat.experiment, file = "loop3")
 counter=1
 
 
 
 
 
-updata.data <- function
-{
-  new.data <- list(side=side, lambda = lambda,N = side * side, D = dist.matrix(side), y = y)
-  new.data$row     <- row.coords <- rep(1:side, times=side)
-  new.data$col     <- col.coords <- rep(1:side, each=side)
-  new.data$row.col <- data.frame(new.data$row, new.data$col)
-  new.data$N       <- side*side
-  new.data$group   <- as.factor(rep(1, new.data$N))
-  new.data$rows    <- new.data$row.col[,1]
-  new.data$cols    <- new.data$row.col[,2]
-  return(new.data)
-}
 
-new.data <- list(side=side, lambda = lambda,N = side * side, D = dist.matrix(side), y = y)
-new.data$row     = row.coords <- rep(1:side, times=side)
-new.data$col     = col.coords <- rep(1:side, each=side)
-new.data$row.col = data.frame(new.data$row, new.data$col)
-new.data$N      = side*side
-n= side*side
-# new.data1        = data.frame(resp = my.data$y)
-new.data$group   <- as.factor(rep(1, new.data$N))
-new.data$rows    <- new.data$row.col[,1]
-new.data$cols    <- new.data$row.col[,2]
 
-new.data1 = data.frame(resp = new.data$y)
-new.data1$pos    <- numFactor(new.data$col, new.data$row.col)
-new.data1$group  <- factor(rep(1, new.data$N))
-new.data1$x      <- new.data$row.col
-new.data1$y      <- new.data$col
+
+
+
+
+
+
 
 
 
