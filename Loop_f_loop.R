fdcc59e0f9566851c1139b61db9f6f43ebc4b610
EttnerAndreas <andi.ettner@yahoo.de>
2020-09-24 13:07:28 +0200

    Job-Method working

--- Loop_f_loop.R
+++ Loop_f_loop.R
@@ -19,16 +19,25 @@ library(R2jags)
 library(mvtnorm)
 
 
-side = 10
 global.mu = 0
-lambda = 0.2 
+methods = c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN","Brent")
+#row.coords <- rep(1:side, times=side)
+#col.coords <- rep(1:side, each=side)
+#row.col <- data.frame(row.coords, col.coords)
+#D <- dist(row.col, method="euclidean", diag=TRUE, upper=TRUE)
+#D <- as.matrix(D)
+#SIGMA <- exp(-lambda*D)
+#mu <- rep(global.mu, times=side*side)
+# sampling from the multivariate normal distribution
+#M <- matrix(nrow=side, ncol=side)
+#M[] <- rmvnorm(1, mu, SIGMA)
 
-repeat.experiment <- data.frame(matrix(NA,500,12))
-#repeat.experiment = data.frame(matrix(NA,500,12))
+
+
+repeat.experiment = data.frame(matrix(NA,1500,12))
 colnames(repeat.experiment) = c("rep", "Lambda", "Side", "glmm_Time", "glmm_Lambda", "glmm_Intercept", "gls_Time", "gls_Lambda", "gls_Intercept", "optim_Time", "optim_Lambda", "optim_Intercept")
 
 
-
 dist.matrix <- function(side)
 {
   row.coords <- rep(1:side, times=side)
@@ -39,15 +48,6 @@ dist.matrix <- function(side)
   return(D)
 }
 
-dist.matrix <- function(side)
-{
-  row.coords <- rep(1:side, times=side)
-  col.coords <- rep(1:side, each=side)
-  row.col <- data.frame(row.coords, col.coords)
-  D <- dist(row.col, method="euclidean", diag=TRUE, upper=TRUE)
-  D <- as.matrix(D)
-  return(D)
-}
 
 cor.surface <- function(side, global.mu, lambda)
 {
@@ -61,40 +61,10 @@ cor.surface <- function(side, global.mu, lambda)
   return(M) # list(...)
 }
 
-row <- row.coords <- rep(1:side, times=side)
-col <- col.coords <- rep(1:side, each=side)
-row.col <<- data.frame(row, col)
-D1 <- dist(row.col, method="euclidean", diag=TRUE, upper=TRUE)
-D <- as.matrix(D1)
-dist.matrix <- function(side)
-{
-  row.coords=row.coords
-  col.coords=col.coords
-  row.col=row.col
-  D=D
-  return(D)
-}
-
-side=10
-global.mu = 0
-lambda = 0.2 
-methods = c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN","Brent")
-M <- cor.surface(side = side, lambda = lambda, global.mu = global.mu)
-jag1 <- as.vector(as.matrix(M))
-my.data <- list(N = side * side, D = dist.matrix(side), y = jag1)
-
-repeat.experiment <- data.frame(matrix(NA,500,12))
-repeat.experiment = data.frame(matrix(NA,500,12)
-                               )
-colnames(repeat.experiment) = c("rep", "Lambda", "Side", "glmm_Time", "glmm_Lambda", "glmm_Intercept", "gls_Time", "gls_Lambda", "gls_Intercept", "optim_Time", "optim_Lambda", "optim_Intercept")
-
-
-
-
 
 
 counter=1
-for (g in 1:5 ){
+for (g in 1:15 ){
   time_repeat = 
     system.time({
       #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
@@ -115,49 +85,56 @@ for (g in 1:5 ){
                   s.r <- side
                   # Loop, ändere side von 4 in 2er schritten auf 20 (daten ergeben sich aus side*side )
                   #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-                  D <- cor.surface(side = side, lambda = lambda, global.mu = global.mu)
-                  M <- cor.surface(side = side, lambda = lambda, global.mu = global.mu)
-                  y <- as.vector(as.matrix(M))
-                  #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-                  new.data1 = 0
-                  new.data = 0
-                  new.data <- list(side=side, lambda = lambda,N = side * side, D = dist.matrix(side), y = y)
-                  new.data$row     = row.coords <- rep(1:side, times=side)
-                  new.data$col     = col.coords <- rep(1:side, each=side)
-                  new.data$row.col = data.frame(new.data$row, new.data$col)
-                  new.data$N      = side*side
-                  n= side*side
-                 # new.data1        = data.frame(resp = my.data$y)
-                  new.data$group   <- as.factor(rep(1, new.data$N))
-                  new.data$rows    <- new.data$row.col[,1]
-                  new.data$cols    <- new.data$row.col[,2]
-                  new.data1 = data.frame(resp = new.data$y)
-                  new.data1$pos    <- numFactor(new.data$col, new.data$row.col)
-                  new.data1$group  <- factor(rep(1, new.data$N))
-                  new.data1$x      <- new.data$row.col
-                  #new.data1$y      <- new.data$col
-                  new.data1$y      <- new.data$row.col
+                  # new.data1$y      <- new.data$row.col
+                  # data$x = row.coords
+                  # data$y = col.coords
                   
                   #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-                  for(i in 1:50){
-                    data
+
+                    
+                    D <- dist.matrix(side)
+                    M <- cor.surface(side = side, lambda = lambda, global.mu = global.mu)
+                    y <- as.vector(as.matrix(M))
+                    #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                    new.data1 = 0
+                    new.data = 0
+                    new.data <- list(side=side, lambda = lambda,N = side * side, D = dist.matrix(side), y = y)
+                    new.data$row     = row.coords <- rep(1:side, times=side)
+                    new.data$col     = col.coords <- rep(1:side, each=side)
+                    new.data$row.col = data.frame(new.data$row, new.data$col)
+                    new.data$N      = side*side
+                    n= side*side
+                    # new.data1        = data.frame(resp = my.data$y)
+                    new.data$group   <- as.factor(rep(1, new.data$N))
+                    new.data$rows    <- new.data$row.col[,1]
+                    new.data$cols    <- new.data$row.col[,2]
+                    new.data1 = data.frame(resp = new.data$y)
+                    new.data1$pos    <- numFactor(new.data$col, new.data$row.col)
+                    new.data1$group  <- factor(rep(1, new.data$N))
+                    new.data1$x      <- new.data$row
+                    new.data1$y      <- new.data$col
+                    
+
                     time_glmmTMB = 
                       system.time({
                         fit.exp <- glmmTMB(resp ~ 1 + exp(pos + 0 | group), data = new.data1)
                       })
+                    
                     time_gls = 
                       system.time({
-                        gls <- gls(y ~ 1, correlation=corExp (form =~ rows + cols), data = new.data)
+                        # gls <- gls(y ~ 1, correlation=corExp (form =~ rows + cols), data = new.data)
+                        gls <- lme(y ~ 1, random=~ 1 | group, correlation=corExp(form=~rows+cols), data = new.data)
                       })
+                    
                     time_optim = 
                       system.time({
                         ll = function(par) {
                           cov = (exp(-par[1]* new.data$D))
-                          -mvtnorm::dmvnorm(new.data1$y, mean = rep(par[2], side*side), sigma = cov ,log = TRUE)
+                          -mvtnorm::dmvnorm(new.data1$resp, mean = rep(par[2], side*side), sigma = cov ,log = TRUE)
                         }
                         result <- optim(par = c(0.5,10),  fn = ll, gr = NULL, method = methods[1], hessian = FALSE)
                       })
-                  }
+                  
                   repeat.experiment[counter, 1] <- g
                   repeat.experiment[counter, 2] <- lambda
                   repeat.experiment[counter, 3] <- s.r 
@@ -168,9 +145,9 @@ for (g in 1:5 ){
                   
                   repeat.experiment[counter, 7] <- time_gls[3]
                   repeat.experiment[counter, 8] <- 1/coef(gls$modelStruct$corStruct, unconstrained = F)
-                  repeat.experiment[counter, 9] <- summary(gls)$coefficients
+                  repeat.experiment[counter, 9] <- summary(gls)$coefficients$fixed
                   
-                  repeat.experiment[counter, 10]  <- time_optim[3]
+                  repeat.experiment[counter, 10] <- time_optim[3]
                   repeat.experiment[counter, 11] <- result$par[1] 
                   repeat.experiment[counter, 12] <- result$par[2]
                   
@@ -188,15 +165,11 @@ for (g in 1:5 ){
       
       #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
     }
-    )}
+)}
 
 repeat.experiment
-saveRDS(repeat.experiment, file = "loop3")
+saveRDS(repeat.experiment, file = "loop4")
 counter=1
-
-
-
-
 
 
 
